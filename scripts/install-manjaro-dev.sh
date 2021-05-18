3740b1d9552593bd3075c1a7d0383b0962312980
David Rotert <david@david-manajaro-ThinkPadX270.fritz.box>
2021-05-18 19:00:03 +0200

    Manjaro install script

---
+++ scripts/install-manjaro-dev.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xfce4-panel --quit
+kill xfconfd
+ansible-playbook --ask-become --extra-vars "config_user=$(whoami) copy_configs=false install_plasma=true" ansible-configs/playbooks/manjaro-development-environment.yml
