499f8fbf5b2a0d6f1b667f9f2fcfd85d0df267a0
David Rotert <d.rotert@outlook.de>
2021-06-25 12:15:57 +0200

    Repair auto install

--- scripts/install-manjaro-dev.sh
+++ scripts/install-manjaro-dev.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+COPY_CONFIGS=true
+
 xfce4-panel --quit
 kill xfconfd
-ansible-playbook --ask-become --extra-vars "config_user=$(whoami) copy_configs=false install_plasma=true" ansible-configs/playbooks/manjaro-development-environment.yml
+ansible-playbook --ask-become --extra-vars "config_user=$(whoami) copy_configs=$COPY_CONFIGS" ansible-configs/playbooks/manjaro-development-environment.yml
 xfce4-panel &
