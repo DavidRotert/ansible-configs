1d97fa944b20a567f26649e6e26c20977173aa3d
David Rotert <d.rotert@outlook.de>
2021-07-05 13:05:19 +0200

    Kill xfconfd in install script

--- scripts/install-manjaro-dev.sh
+++ scripts/install-manjaro-dev.sh
@@ -3,6 +3,6 @@ COPY_CONFIGS=true
 CONFIG_USER=$(whoami)
 
 xfce4-panel --quit
-kill xfconfd
+killall xfconfd
 ansible-playbook --ask-become --extra-vars "config_user=$CONFIG_USER copy_configs=$COPY_CONFIGS" ansible-configs/playbooks/manjaro-development-environment.yml
 xfce4-panel &
