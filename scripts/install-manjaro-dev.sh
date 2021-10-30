de18929e43c4b885884c83d82f76420d9b1cee84
David Rotert <d.rotert@outlook.de>
2021-10-30 12:02:43 +0200

    MacOS Windows

--- scripts/install-manjaro-dev.sh
+++ scripts/install-manjaro-dev.sh
@@ -2,6 +2,8 @@
 COPY_CONFIGS=true
 CONFIG_USER=$(whoami)
 
+sudo pamac install ansible ansible-lint
+
 xfce4-panel --quit
 killall xfconfd
 ansible-playbook --ask-become --extra-vars "config_user=$CONFIG_USER copy_configs=$COPY_CONFIGS" ansible-configs/playbooks/manjaro-development-environment.yml
