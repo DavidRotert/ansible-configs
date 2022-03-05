c0a3936d8e2ac424b23add9f4c86626a229caf1a
David Rotert <d.rotert@outlook.de>
2022-03-05 11:30:01 +0100

    Remove Eric from Python app updater

--- files/local/bin/update-python-apps.sh
+++ files/local/bin/update-python-apps.sh
@@ -5,4 +5,3 @@ echo Updating Python apps in /opt ...
 /opt/xonsh/bin/pip install --upgrade pip xonsh[full] setproctitle jedi
 /opt/spyder/bin/pip install --upgrade pip spyder
 /opt/gdbgui/bin/pip install --upgrade pip gdbgui
-/opt/eric/bin/pip install --upgrade pip eric-ide
