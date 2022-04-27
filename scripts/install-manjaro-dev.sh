2cdd0f8b65f462ad19be26223f26153b4dde4acf
David Rotert <d.rotert@outlook.de>
2022-04-27 12:11:12 +0200

    Change install script

--- scripts/install-manjaro-dev.sh
+++ scripts/install-manjaro-dev.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 COPY_CONFIGS=true
+DESKTOP=desktop
 CONFIG_USER=$(whoami)
 
 sudo pamac install ansible ansible-lint
 
 xfce4-panel --quit
 killall xfconfd
-ansible-playbook --ask-become --extra-vars "config_user=$CONFIG_USER copy_configs=$COPY_CONFIGS" ansible-configs/playbooks/manjaro-development-environment.yml
+
+ansible-playbook --ask-become --extra-vars "desktop=$DESKTOP config_user=$CONFIG_USER copy_configs=$COPY_CONFIGS" ansible-configs/playbooks/manjaro-development-environment.yml
+
 xfce4-panel &
