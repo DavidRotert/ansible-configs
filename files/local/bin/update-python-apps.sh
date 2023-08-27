575df805d4135904e2fed4eb83c7e82f98c5ea67
David Rotert <d.rotert@outlook.de>
2023-08-27 17:55:56 +0200

    Install Python virtualenvs

--- files/local/bin/update-python-apps.sh
+++ files/local/bin/update-python-apps.sh
@@ -2,6 +2,11 @@
 
 echo Updating Python apps in /opt ...
 
-/opt/xonsh/bin/pip install --upgrade pip xonsh[full] setproctitle jedi
-/opt/spyder/bin/pip install --upgrade pip spyder
-/opt/gdbgui/bin/pip install --upgrade pip gdbgui
+sudo python3 -m venv --upgrade /opt/xonsh
+sudo /opt/xonsh/bin/pip install --upgrade pip xonsh[full] setproctitle jedi
+
+sudo python3 -m venv --upgrade /opt/spyder
+sudo /opt/spyder/bin/pip install --upgrade pip spyder
+
+sudo python3 -m venv --upgrade /opt/gdbgui
+sudo /opt/gdbgui/bin/pip install --upgrade pip gdbgui
